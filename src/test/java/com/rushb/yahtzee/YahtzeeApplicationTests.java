35f64f6223ab135af29408ad289bac6cd1da762a
Jarod Acloque <jarod.acloque@live.fr>
2023-04-14 15:08:54 +0200

    Spring init

---
+++ src/test/java/com/rushb/yahtzee/YahtzeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushb.yahtzee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahtzeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
